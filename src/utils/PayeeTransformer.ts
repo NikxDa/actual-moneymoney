48818ed247af53e10a6b6b5ae4541e565c468955
Jens Bergmann <1742418+1cu@users.noreply.github.com>
2025-03-31 17:56:14 +0200

    feat: update OpenAI API to v4 and remove axios dependency (#28)

--- src/utils/PayeeTransformer.ts
+++ src/utils/PayeeTransformer.ts
@@ -1,21 +1,19 @@
-import { Configuration, OpenAIApi } from 'openai';
+import OpenAI from 'openai';
 
 class PayeeTransformer {
-    private openai: OpenAIApi;
+    private openai: OpenAI;
 
     constructor(openAiApiKey: string) {
-        const configuration = new Configuration({
+        this.openai = new OpenAI({
             apiKey: openAiApiKey,
         });
-
-        this.openai = new OpenAIApi(configuration);
     }
 
     public async transformPayees(payeeList: string[]) {
         const prompt = this.generatePrompt();
 
         try {
-            const response = await this.openai.createChatCompletion({
+            const response = await this.openai.chat.completions.create({
                 model: 'gpt-3.5-turbo',
                 messages: [
                     { role: 'system', content: prompt },
@@ -24,7 +22,7 @@ class PayeeTransformer {
                 temperature: 0,
             });
 
-            const output = response.data.choices[0].message?.content as string;
+            const output = response.choices[0].message?.content as string;
             return JSON.parse(output) as { [key: string]: string };
         } catch (e) {
             return null;
@@ -34,17 +32,17 @@ class PayeeTransformer {
     private generatePrompt() {
         return `
             You are now a model trained to classify bank account transactions. You will receive
-            a list of payees as they appear in the source transactions, and you will return 
-            a cleaned up, human-readable version of the payees. Return a JSON formatted object, where 
-            the old payee names are mapped to the new ones. Make sure the new payee names are clear 
+            a list of payees as they appear in the source transactions, and you will return
+            a cleaned up, human-readable version of the payees. Return a JSON formatted object, where
+            the old payee names are mapped to the new ones. Make sure the new payee names are clear
             and concise, and omit any unnecessary details. For example, if the payee is "Amazon.com",
             you should return "Amazon". If the payee is "AMAZON.COM/BILLWA", you should also return
-            "Amazon". You are free to make some assumptions about the payees. If you don't know the 
+            "Amazon". You are free to make some assumptions about the payees. If you don't know the
             payee, return "Unknown". If for some reason you cannot create a JSON object, return {}.
 
             Examples:
 
-            Input: - 
+            Input: -
             Output: {}
 
             Input: Amzn Mktp US*1234567890
